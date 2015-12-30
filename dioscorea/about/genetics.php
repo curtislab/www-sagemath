7b0933889fdf029224b8c35516f4175e94245d1c
ApriLab <april.hile@curtislab.org>
2015-12-29 19:35:47 -0500

    Site Layout & php
    Laid out ALL basic pages; php integration of header/footer

---
+++ dioscorea/about/genetics.php
@@ -0,0 +1,69 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1348\cocoasubrtf170
+{\fonttbl\f0\fmodern\fcharset0 Courier;\f1\fnil\fcharset0 Consolas;}
+{\colortbl;\red255\green255\blue255;\red234\green234\blue234;\red19\green36\blue126;}
+\margl1440\margr1440\vieww20640\viewh8400\viewkind0
+\deftab720
+\pard\tx720\pardeftab720\sl336
+
+\f0\fs24 \cf0 \cb2 \expnd0\expndtw0\kerning0
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"\cb1 \expnd0\expndtw0\kerning0
+\
+\cb2 \expnd0\expndtw0\kerning0
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">\cb1 \expnd0\expndtw0\kerning0
+\
+\cb2 \expnd0\expndtw0\kerning0
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"> \cb1 \expnd0\expndtw0\kerning0
+\
+\cb2 \expnd0\expndtw0\kerning0
+\
+<head>\cb1 \expnd0\expndtw0\kerning0
+\
+\cb2 \expnd0\expndtw0\kerning0
+	<title>Dioscorea.org</title>\cb1 \expnd0\expndtw0\kerning0
+\
+\cb2 \expnd0\expndtw0\kerning0
+	<meta http-equiv="description" content="page description" />\cb1 \expnd0\expndtw0\kerning0
+\
+\cb2 \expnd0\expndtw0\kerning0
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />\cb1 \expnd0\expndtw0\kerning0
+\
+\cb2 \expnd0\expndtw0\kerning0
+	<style type="text/css">@import "styles.css";</style>\cb1 \expnd0\expndtw0\kerning0
+\
+\cb2 \expnd0\expndtw0\kerning0
+</head>\cb1 \kerning1\expnd0\expndtw0 \
+\cb2 \expnd0\expndtw0\kerning0
+\
+<body>\cb1 \expnd0\expndtw0\kerning0
+\
+\cb2 \expnd0\expndtw0\kerning0
+<?php include("www/dioscorea/includes/header.html");?>\cb1 \expnd0\expndtw0\kerning0
+\
+\
+\pard\pardeftab720
+\cf0 \expnd0\expndtw0\kerning0
+<h2>Genetics</h2>\
+  <p>Relevant External Sources\
+    <ul>\
+      <li><a href="http://www.ncbi.nlm.nih.gov/nuccore">Genbank</a></li>\
+      <li><a href="https://gold.jgi.doe.gov/project?id=5683">JGI Gold Program's Transcriptome of <i>Dioscorea alata</i></a></li>\
+      <li><a href="http://biblio.iita.org/documents/U14ArtMuthamiaDeterminationNothomDev.pdf-f341ad80e81d5304834f93bc8667ea55.pdf">Determination of ploidy among Yam (<i>Dioscorea spp.</i>) landraces in Kenya by flow cytometry</a></li>\
+    </ul>\
+  </p>\
+\pard\pardeftab720\sl364
+
+\f1 \cf3 \expnd0\expndtw0\kerning0
+\
+\pard\tx720\pardeftab720\sl336
+
+\f0 \cf0 \cb2 \expnd0\expndtw0\kerning0
+<?php include("www/dioscorea/includes/footer.html");?>\cb1 \expnd0\expndtw0\kerning0
+\
+\
+\cb2 \expnd0\expndtw0\kerning0
+\
+</body>\cb1 \expnd0\expndtw0\kerning0
+\
+\cb2 \expnd0\expndtw0\kerning0
+\
+</html>}
---
+++ dioscorea/propagation/where.php
@@ -0,0 +1,79 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1348\cocoasubrtf170
+{\fonttbl\f0\fmodern\fcharset0 Courier;\f1\fnil\fcharset0 Consolas;}
+{\colortbl;\red255\green255\blue255;\red234\green234\blue234;\red19\green36\blue126;\red38\green38\blue38;
+}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\tx720\pardeftab720\sl336
+
+\f0\fs24 \cf0 \cb2 \expnd0\expndtw0\kerning0
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"\cb1 \expnd0\expndtw0\kerning0
+\
+\cb2 \expnd0\expndtw0\kerning0
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">\cb1 \expnd0\expndtw0\kerning0
+\
+\cb2 \expnd0\expndtw0\kerning0
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"> \cb1 \expnd0\expndtw0\kerning0
+\
+\cb2 \expnd0\expndtw0\kerning0
+\
+<head>\cb1 \expnd0\expndtw0\kerning0
+\
+\cb2 \expnd0\expndtw0\kerning0
+<title>Dioscorea.org</title>\cb1 \expnd0\expndtw0\kerning0
+\
+\cb2 \expnd0\expndtw0\kerning0
+<meta http-equiv="description" content="page description" />\cb1 \expnd0\expndtw0\kerning0
+\
+\cb2 \expnd0\expndtw0\kerning0
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />\cb1 \expnd0\expndtw0\kerning0
+\
+\cb2 \expnd0\expndtw0\kerning0
+<style type="text/css">@import "styles.css";</style>\cb1 \expnd0\expndtw0\kerning0
+\
+\cb2 \expnd0\expndtw0\kerning0
+</head>\cb1 \kerning1\expnd0\expndtw0 \
+\cb2 \expnd0\expndtw0\kerning0
+\
+<body>\cb1 \expnd0\expndtw0\kerning0
+\
+\cb2 \expnd0\expndtw0\kerning0
+<?php include("www/dioscorea/includes/header.html");?>\cb1 \expnd0\expndtw0\kerning0
+\
+\
+
+\itap1\trowd \taflags1 \trgaph108\trleft-108 \trcbpat1 \trbrdrt\brdrnil \trbrdrl\brdrnil \trbrdrt\brdrnil \trbrdrr\brdrnil 
+\clmgf \clvertalt \clshdrawnil \clwWidth23130\clftsWidth3 \clminw1000 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrs\brdrw20\brdrcf2 \clpadl200 \clpadr200 \gaph\cellx4320
+\clmrg \clvertalt \clshdrawnil \clwWidth23130\clftsWidth3 \clminw1000 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrs\brdrw20\brdrcf2 \clpadl200 \clpadr200 \gaph\cellx8640
+\pard\intbl\itap1\pardeftab720\sl364
+
+\f1 \cf0 \expnd0\expndtw0\kerning0
+<h2>Climate & Regions</h2>\
+<p>Dioscorea grows throughout tropical regions around the world. Though dependent on species/genus, most species originated in either Southeast Asia or Africa. In some places in North America, species of Dioscorea are considered an invasive species.</p>\
+<p>Relevant External Sources\
+   <ul>\
+       <li><a href=\'93http://bonap.net/NAPA/TaxonMaps/Genus/County/Dioscorea\'94>13 common species growth in the U.S. according to BONAP\'92s North American Plant Atlas </a></li>\
+       <li><a href=\'93{\field{\*\fldinst{HYPERLINK "http://www.ars.usda.gov/research/projects/projects.htm?accn_no=427679"}}{\fldrslt http://www.ars.usda.gov/research/projects/projects.htm?accn_no=427679}}\'94>Invasive Plant Research Laboratory\'92s Project on biological control of Dioscorea bulbifera</a></li>\
+   </ul>\
+</p>\cf3 \expnd0\expndtw0\kerning0
+\
+\
+\pard\intbl\itap1\tx720\pardeftab720\sl336
+
+\f0 \cf0 \cb2 \expnd0\expndtw0\kerning0
+<?php include("www/dioscorea/includes/footer.html");?>
+\f1 \cf4 \cb1 \expnd0\expndtw0\kerning0
+\cell 
+\pard\intbl\itap1\cell \lastrow\row
+\pard\tx720\pardeftab720\sl336
+
+\f0 \cf0 \expnd0\expndtw0\kerning0
+\
+\
+\cb2 \expnd0\expndtw0\kerning0
+\
+</body>\cb1 \expnd0\expndtw0\kerning0
+\
+\cb2 \expnd0\expndtw0\kerning0
+\
+</html>}
