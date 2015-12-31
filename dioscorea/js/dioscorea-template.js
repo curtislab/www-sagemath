bca313e57310a17015c98a519e3c4854b2f9a41b
Brandon Curtis <brandon.curtis@gmail.com>
2015-12-31 12:44:09 -0500

    templated footer with js

---
+++ dioscorea/js/dioscorea-template.js
@@ -0,0 +1,6 @@
+function dioscoreaFooter() {
+	document.write('<div id="footer-wrap">' +
+		'<a href="http://curtislab.org">A CurtisLab Production</a>, ' +
+		'under a <a href="http://creativecommons.org/licenses/by-sa/4.0/">' +
+		'Creative Commons Attribution-ShareAlike 4.0 License</a></p></div>')
+	}
